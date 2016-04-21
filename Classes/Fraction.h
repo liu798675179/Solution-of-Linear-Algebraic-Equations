37f2048238ba2f34b487bbda3f445912a34c94cd
Michael Liu <chyliu14@lzu.edu.cn>
2016-04-21 18:03:19 +0800

    Update Fraction.h

--- Classes/Fraction.h
+++ Classes/Fraction.h
@@ -3,6 +3,8 @@
 
 #include <iostream>
 
+using std::string;
+
 class Fraction
 {
 public:
@@ -24,22 +26,23 @@ public:
 	friend bool operator> (const Fraction&, const Fraction&);
 	friend bool operator>= (const Fraction&, const Fraction&);
 
+	string cout_temp_addition_for_transmission() const;                                      // Temporary output.
 	static Fraction Frac_abs(Fraction &);                                                    // Absolute value of a fraction.
-        void cin_num(long long);
-	void cin_den(long long);
+	void cin_num(long long);                                                                 // Input a Fraction's numerator.
+	void cin_den(long long);                                                                 // Input a Fraction's denminator.
 public:
 	Fraction() = default;                                                                    // constructor function: numerator = 0, denominator = 1
 	Fraction(long long temp_a) : numerator(temp_a) { }                                       // constructor function: numerator = temp_a, denominator = 1.
 	Fraction(long long temp_a, long long temp_b) : numerator(temp_a), denominator(temp_b) {  // constructor function: numerator = temp_a, denominator = 1 and reduction of a fraction.
 		rof();
 	}
-	~Fraction() { };                                                                         // destructor
+	~Fraction() { };                                                                         // Destructor
 
 private:
 	long long numerator = 0;
 	long long denominator = 1;
 
-	long long gcd(long long, long long);                                                     // greatest common divisor
+	long long gcd(long long, long long);                                                     // Greatest common factor
 	void rof();                                                                              // reduction of a fraction
 };
 
