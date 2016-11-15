f0cc5e188ff0d8cff5cb612f0ec21accd24ba8cb
Michael Liu <chyliu14@lzu.edu.cn>
2016-11-15 11:10:35 +0800

    Update Fraction.h

--- Classes/Fraction.h
+++ Classes/Fraction.h
@@ -26,24 +26,33 @@ public:
 	friend bool operator> (const Fraction&, const Fraction&);
 	friend bool operator>= (const Fraction&, const Fraction&);
 
-	string cout_temp_addition_for_transmission() const;                                      // Temporary output.
-	static Fraction Frac_abs(Fraction &);                                                    // Absolute value of a fraction.
-	void cin_num(long long);                                                                 // Input a Fraction's numerator.
-	void cin_den(long long);                                                                 // Input a Fraction's denminator.
+	// Temporary output.
+	string cout_temp_addition_for_transmission() const;
+	// Absolute value of a fraction.
+	static Fraction Frac_abs(Fraction &);
+	// Input a Fraction's numerator.
+	void cin_num(long long);
+	// Input a Fraction's denminator.
+	void cin_den(long long);
 public:
-	Fraction() = default;                                                                    // constructor function: numerator = 0, denominator = 1
-	Fraction(long long temp_a) : numerator(temp_a) { }                                       // constructor function: numerator = temp_a, denominator = 1.
-	Fraction(long long temp_a, long long temp_b) : numerator(temp_a), denominator(temp_b) {  // constructor function: numerator = temp_a, denominator = 1 and reduction of a fraction.
+	// constructor function: numerator = 0, denominator = 1
+	Fraction() = default;
+	// constructor function: numerator = temp_a, denominator = 1.
+	Fraction(long long const &temp_a) : numerator(temp_a) { }
+	// constructor function: numerator = temp_a, denominator = 1 and reduction of a fraction.
+	Fraction(long long const &temp_a, long long const &temp_b) : numerator(temp_a), denominator(temp_b) {
 		rof();
 	}
-	~Fraction() = default;                                                                   // Destructor
+	~Fraction() = default;
 
 private:
 	long long numerator = 0;
 	long long denominator = 1;
 
-	long long gcd(long long, long long);                                                     // Greatest common factor
-	void rof();                                                                              // reduction of a fraction
+	// Greatest common factor.
+	static long long gcd(long long, long long);
+	// reduction of a fraction.
+	void rof();
 };
 
 #endif
